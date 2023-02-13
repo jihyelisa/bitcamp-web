134769a94b86bb18cf730d0e8d9ae415d85c4e2b
jihyelisa <jihyelisak@gmail.com>
2023-02-13 09:35:59 +0900

    weekend hw

--- 05_class/src/class__/Exam.java
+++ 05_class/src/class__/Exam.java
@@ -1,5 +1,19 @@
 package class__;
 
+import java.util.Scanner;
+
 public class Exam {
+	Scanner scan = new Scanner(System.in);
+	
+	private String name = null;
+	private String dap = null;
+	private char[] ox = null;
+	private int score = 0;
+	private final String JUNG = "11111"; //상수화
+	
+	public void compare() {
+		
+	}
+	
 
 }
