18bca608654598bb0246e4b6693b215ecba0509f
jihyelisa <jihyelisak@gmail.com>
2023-03-01 15:12:43 +0900

    weekend hw

--- java_ee/guestbookServlet/src/main/java/guestbook/service/GuestbookSearchServlet.java
+++ java_ee/guestbookServlet/src/main/java/guestbook/service/GuestbookSearchServlet.java
@@ -17,37 +17,27 @@ public class GuestbookSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		
 	//데이터
 	request.setCharacterEncoding("UTF-8");
-	String name = request.getParameter("name");
-	String email = request.getParameter("email");
-	String homepage = request.getParameter("homepage");
-	String subject = request.getParameter("subject");
-	String content = request.getParameter("content");
-	
-	GuestbookDTO guestbookDTO = new GuestbookDTO();
-	guestbookDTO.setName(name);
-	guestbookDTO.setEmail(email);
-	guestbookDTO.setHomepage(homepage);
-	guestbookDTO.setSubject(subject);
-	guestbookDTO.setContent(content);
+	String id = request.getParameter("id");
+	String pwd = request.getParameter("pwd");
 	
 	//DB
-	GuestbookDAO guestbookDAO = GuestbookDAO.getInstance();
-	int su = guestbookDAO.guestbookWrite(guestbookDTO);
+	MemberDAO memberDAO = MemberDAO.getInstance();
+	boolean isMember = memberDAO.memberCheck(id, pwd);
 
 	//응답
 	response.setContentType("text/html; charset=UTF-8");
 	PrintWriter out = response.getWriter();
 	out.println("<html>");
 	out.println("<body>");
-	if (su==0) {
-		out.println("<h3>방명록에 등록 실패</h3>");
+	if (isMember==false) {
+		out.println("<h3>로그인 실패</h3>");
+		//이전 버튼
 		out.println("<input type='button' value='← 이전' onclick='history.go(-1)'>");
 	}
-	else out.println("<h3>방명록에 등록 성공</h3>");
-	out.println("<input type='button' value='← 이전' onclick='history.go(-1)'>");
+	else out.println("<h3>로그인 성공</h3>");
 	out.println("</body>");
 	out.println("</html>");
 	}
